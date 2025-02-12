8a01e0d3dd53297a4bc50caff13c53e1b4ef23aa
Siddhant Kapoor <siddhantkapoor2025@gmail.com>
2025-02-12 10:57:17 +0530

    updated

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import mysql from "mysql2/promise";
 import express from "express"
 import cors from 'cors'
 import dotenv from 'dotenv'
+import bodyParser from "body-parser";
 dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 const PORT = process.env.PORT || 3000;
 
